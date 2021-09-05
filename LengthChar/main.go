0755fbeac4ef3e17549866a5d84e7cdc5565d137
Fauzan Amir Al Ghiffary <fauzanamiralghiffary@MacBook-Pro-Fauzan.local>
2021-09-05 21:12:51 +0700

    ada update dikit

--- LengthChar/main.go
+++ LengthChar/main.go
@@ -7,16 +7,6 @@ import (
 	"unicode"
 )
 
-func removeSpace(s string) string {
-    rr := make([]rune, 0, len(s))
-    for _, r := range s {
-        if !unicode.IsSpace(r) {
-            rr = append(rr, r)
-        }
-    }
-    return string(rr)
-}
-
 func main() {
 
 	fmt.Print("Tulis Kalimat :")
